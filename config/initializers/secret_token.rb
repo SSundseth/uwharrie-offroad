739e79b51bc9c6e02e37e964e28cdd5dc1e28771
Stefan Sundseth <stefan.sundseth@viget.com>
2012-07-14 20:02:36 -0400

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UwharrieOffroad::Application.config.secret_token = '7ed65f1161454be1762bbf52ece7c0adfa0be5ca0182810d8d9d9800a822d6e85454325ed45060ff92cd0fb4cb7b8d2b3068fef603f364d42aabf4859c7598f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UwharrieOffroad::Application.config.session_store :cookie_store, key: '_uwharrie-offroad_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UwharrieOffroad::Application.config.session_store :active_record_store
