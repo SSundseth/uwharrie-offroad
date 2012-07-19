d0de9ff2a3390c30b393aecf70dc5bca346e931c
Stefan Sundseth <stefan.sundseth@viget.com>
2012-07-19 15:03:53 -0400

    updated styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ UwharrieOffroad::Application.routes.draw do
   resource :account, :only => [:edit, :update], :controller => :users
 
   match 'reset_password' => 'password_resets#new'
-  match 'signup' => 'users#new'
-  match 'logout' => 'sessions#destroy'
-  match 'login'  => 'sessions#new'
+  match 'signup'         => 'users#new'
+  match 'logout'         => 'sessions#destroy'
+  match 'login'          => 'sessions#new'
+  match 'about'          => 'pages#about'
   root :to => "trails#index"
 end
