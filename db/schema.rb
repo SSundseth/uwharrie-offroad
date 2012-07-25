8803bb80d011f2e5a1f77a848029a0a453f12f35
Stefan Sundseth <stefan.sundseth@viget.com>
2012-07-25 17:11:22 -0400

    added info field for trails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120718184449) do
+ActiveRecord::Schema.define(:version => 20120725204325) do
 
   create_table "timings", :force => true do |t|
     t.integer  "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120718184449) do
     t.string   "difficulty"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "info"
   end
 
   create_table "users", :force => true do |t|
